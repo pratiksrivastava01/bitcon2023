30db1d669a622f2144332e0880a0b2a2fd02c0c3
Pratik <srivastavapratik35@gmail.com>
2023-12-02 17:51:55 +0530

    ui

--- pages/PaperSubmission/info.jsx
+++ pages/PaperSubmission/info.jsx
@@ -22,6 +22,8 @@ const info = () => {
           content="HTML, CSS, JavaScript, ReactJS, NextJS, TailwindCSS, HnCC, Hackathon & Coding Club, BIT Sindri, Dhanbad"
         />
       </Head>
+      <Navbar />
+      <Nav3 />
 
       <div className="flex flex-row items-center sm:flex-col mt-6 justify-center">
         <div className="h-auto w-[80%] rounded-lg p-4  border-primary shadow-2xl  ">
@@ -69,128 +71,6 @@ const info = () => {
                 ensuring the originality of content using iThenticate with an
                 acceptable limit of 30% including References.
               </p>
-            </li>
-            <li>
-              <p>
-                <span className="font-bold text-lg">
-                  3. Headers, footers, and page numbers:{" "}
-                </span>
-                No headers, footers, or page numbers should appear on your
-                manuscript.
-              </p>
-            </li>
-            <li>
-              <p>
-                <span className="font-bold text-lg">4. Margins:</span>
-                Papers cannot be accepted with margins smaller than those given
-                in the table below:
-              </p>
-              <table className=" ml-[2rem] sm:ml-[-1rem]">
-                <thead className="gap-[5rem]">
-                  <tr className="p-4">
-                    <th className="bg-blue text-center text-primary-light">
-                      Margin
-                    </th>
-                    <th className="bg-blue text-center text-primary-light">
-                      US Letter
-                    </th>
-                    <th className="bg-blue text-center text-primary-light">
-                      A4
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Top</td>
-                    <td>0.75 inch</td>
-                    <td>19 mm</td>
-                  </tr>
-                  <tr>
-                    <td>Bottom</td>
-                    <td>1 inch</td>
-                    <td>43 mm</td>
-                  </tr>
-                  <tr>
-                    <td>Side</td>
-                    <td>0.625 inch</td>
-                    <td>13 mm</td>
-                  </tr>
-                </tbody>
-              </table>
-            </li>
-            <li>
-              <p>
-                <span className="font-bold text-lg">4. Fonts:</span>
-                Fonts must be either Post Script or True Type and licensed for
-                embedding. Use of unlicensed and custom-made or hacked fonts is
-                strictly prohibited; your submission will be rejected if it is
-                found to contain them. Below is a list of recommended fonts:
-              </p>
-              <table className=" ml-[2rem] sm:ml-[-1rem]">
-                <thead className="gap-[5rem]">
-                  <tr className="p-4">
-                    <th className="bg-blue text-center text-primary-light">
-                      Content
-                    </th>
-                    <th className="bg-blue text-center text-primary-light">
-                      Font
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Text</td>
-                    <td>Times New Roman</td>
-                  </tr>
-                  <tr>
-                    <td>Code</td>
-                    <td>Courier</td>
-                  </tr>
-                  <tr>
-                    <td>Equations</td>
-                    <td>Symbol</td>
-                  </tr>
-                </tbody>
-              </table>
-            </li>
-            <li>
-              <span className="font-bold text-lg">5. Graphics and Images:</span>
-              <ul>
-                <li className="flex gap-3">
-                  {" "}
-                  <GoPrimitiveDot className="mt-1" />
-                  Choose vector graphics (e.g., .eps or .pdf) over bitmap (.bmp,
-                  .png, or .jpg) images because they are electronically
-                  efficient and will not conflict with conversions to PDF.{" "}
-                </li>
-                <li className="flex gap-3">
-                  {" "}
-                  <GoPrimitiveDot className="mt-1" />
-                  All images should be checked by printing to hardcopy if the
-                  conference will be producing a book, especially when color
-                  images in your paper will be translated to black and white in
-                  print.{" "}
-                </li>
-                <li className="flex gap-3">
-                  {" "}
-                  <GoPrimitiveDot className="mt-1" />
-                  Resolution should be between 150 and 300 dpi or less.
-                  Resolutions greater than 300 will cause down sampling; images
-                  below 300 will not render well in print.{" "}
-                </li>{" "}
-                <li className="flex gap-3 ml-[-0.2rem]">
-                  <GoPrimitiveDot className="mt-1" />
-                  Labels within graphics should have a type point size of 4 or
-                  higher.{" "}
-                </li>
-                <li className="flex gap-3">
-                  {" "}
-                  <GoPrimitiveDot className="mt-1" />
-                  Smooth Graphics should not be used as it often produces
-                  extremely large files with slow loading time. The Smooth
-                  Graphics option is usually found in the Page Setup Dialog box.
-                </li>
-              </ul>
             </li>
           </ul>
 
@@ -359,7 +239,7 @@ const info = () => {
           </ul> */}
         </div>
       </div>
-      <Foot />
+      {/* <Foot /> */}
     </>
   );
 };
