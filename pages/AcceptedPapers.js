d2bfa91de06bc28c2412ef4476e9269361b87ec8
Shashikanta Mahato <shashikanta2234@gmail.com>
2024-11-30 17:47:25 +0530

    Update AcceptedPapers.js

--- pages/AcceptedPapers.js
+++ pages/AcceptedPapers.js
@@ -36,18 +36,18 @@ const awards = () => {
           </div>
           <div className="mx-[2rem]  rounded-lg justify-center flex flex-col border-primary shadow-2xl sm:mt-0 mt-[0.5rem] sm:mx-[0rem] sm:p-4 sm:h-auto">
             <div className="p-4 flex flex-col gap-2 flex-wrap items-center">
-              <table className="">
+              <table className=" w-[300px]">
                 <thead className="bg-[#002E73] ">
                   <tr>
-                    <th className="p-4 text-white text-center">Sr. No.</th>
-                    <th className="p-4 text-white text-center">Paper ID</th>
+                    <th className="px-2 text-white text-center">Sr. No.</th>
+                    <th className="px-2 text-white text-center">Paper ID</th>
                   </tr>
                 </thead>
                 <tbody className="text-sm sm:text-xs">
                   {papers.map((paper, index) => (
                     <tr key={paper.index}>
-                      <td className="p-4">{paper.s_no}</td>
-                      <td className="p-4">{paper.p_id}</td>
+                      <td className="px-2">{paper.s_no}</td>
+                      <td className="px-2">{paper.p_id}</td>
                     </tr>
                   ))}
                 </tbody>
