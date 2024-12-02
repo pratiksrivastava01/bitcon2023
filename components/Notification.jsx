b349e765daf2b52d489f26bbb718d94eb9b42c0c
Shashikanta Mahato <shashikanta2234@gmail.com>
2024-12-02 13:01:27 +0530

    criteria added

--- components/Notification.jsx
+++ components/Notification.jsx
@@ -4,19 +4,19 @@ import Image from "next/image";
 import notification2 from "./../assets/notification2.svg";
 const notification = () => {
   return (
-    <div className="flex flex-row ">
+    <div className="flex flex-row overflow-scroll">
       {/* <div className="bg-[#002256] p-2 text-[white] sm:text-lg  text-2xl">
         {" "}
         Notification
       </div> */}
-      <div className="bg-[#FAC804] p-1 flex w-full justify-center  overflow-hidden mt-4">
-        <div className={`${styles.animate} flex w-full gap-12`}>
+      <div className="bg-[#FAC804] p-1 flex w-full justify-center sm:w-max  overflow-hidden sm:overflow-auto mt-4">
+        <div className={`${styles.animate} flex w-full sm:w-max gap-12`}>
           {" "}
           {/* <span>1. All Acceptance notifications rolled out.</span>
           <span>
             2. Final Extended date of Registration is <b>20-November-2024</b>
           </span> */}
-          <div className="flex flex-row justify-center items-center">
+          <div className="flex flex-row justify-center w-auto items-center sm:w-max">
             {/* https://drive.google.com/file/d/1-gvUIU1nfRfKhLEvhRRzx1V7ayoE1sqP/view?usp=sharing */}
             <a
               href="/ImportantNotice"
@@ -32,7 +32,7 @@ const notification = () => {
               </span>
             </a>
           </div>
-          <div className="flex flex-row justify-center items-center">
+          <div className="flex flex-row justify-center items-center w-auto sm:w-max">
             {/* https://drive.google.com/file/d/1-gvUIU1nfRfKhLEvhRRzx1V7ayoE1sqP/view?usp=sharing */}
             <a
               href="/ConfirmedRegistrations"
@@ -42,9 +42,25 @@ const notification = () => {
                 src={notification2}
                 className={`${styles.animate2} h-4 w-4 text-black`}
               /> */}
-              <span className="text-black">
+              <span className="text-black ">
                 {" "}
                 Registration Confirmed Paper IDs
+              </span>
+            </a>
+          </div>
+          <div className="flex flex-row justify-center items-center w-auto sm:w-max">
+            {/* https://drive.google.com/file/d/1-gvUIU1nfRfKhLEvhRRzx1V7ayoE1sqP/view?usp=sharing */}
+            <a
+              href="/awards#ac"
+              className="text-black text-base flex flex-row gap-2 justify-start items-center"
+            >
+              {/* <Image
+                src={notification2}
+                className={`${styles.animate2} h-4 w-4 text-black`}
+              /> */}
+              <span className="text-black ">
+                {" "}
+                Criterion for Best Paper Award in Each Track
               </span>
             </a>
           </div>
